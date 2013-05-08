88c2a1d14a02efddfa6c7d380e8c26460b604e68
manfred <manfred.andres@gmail.com>
2013-05-09 01:03:05 +0200

    derty helper now even dertyer.. just reworked to use the SimpleIndex-wrapper

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
 var {Index} = require("lucindex/lib/index");
+var {SimpleIndex} = require("lucindex/lib/simpleindex");
 var analyzerFactory = require("lucindex/lib/analyzer");
 var sa = analyzerFactory.createStandardAnalyzer("LUCENE_42");
-var idx = Index.createIndex("/tmp/", "testindex", sa, {
-    version: "LUCENE_42", 
-    meta: {id: {type: "int", store: true}, count: {type: "float", store: true}, name: {store: true}}
-    });
+var rawidx = Index.createIndex("/tmp/", "testindex", sa, "LUCENE_42");
+var idx = new SimpleIndex(rawidx, {meta: {id: {type: "int", store: true}, count: {type: "float", store: true}, name: {store: true}}});
 print("size: " + idx.size());
 
 idx.removeAll();
 java.lang.Thread.sleep(1000);
 print("size: " + idx.size());
 
-var doc = idx.createDocument({id: 2, name: "hombre", count: 5.3})
+var doc = {id: 2, name: "hombre", count: 5.3};
 
 idx.update("count", 5.3, doc);
 idx.update("count", 5.3, doc);
