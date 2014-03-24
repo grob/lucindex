6940310b53024f5cda3f183a363fc3f1af205e57
manfred <manfred.andres@gmail.com>
2014-03-24 23:22:37 +0100

    no more config.meta... only meta

--- test.js
+++ test.js
@@ -12,11 +12,10 @@ var analyzer = analyzerFactory.createPerFieldAnalyzer({"name": pa, "address": pa
 
 var rawidx = Index.createIndex("/tmp/", "testindex", analyzer, VER);
 var idx = new SimpleIndex(rawidx, 
-        {meta: 
             {defaultField: "name",
              id: {type: "int", store: true}, 
              count: {type: "float", store: true}, 
-             name: {store: true}, address: {store: false}}});
+             name: {store: true}, address: {store: false}});
 print("size: " + idx.size());
 
 idx.removeAll();
@@ -24,9 +23,9 @@ java.lang.Thread.sleep(1000);
 print("size: " + idx.size());
 
 var docs = [{id: 1, name: "bernd", count: 5.1, address: "Mustergasse 8"},
-            {id: 2, name: "tom", count: 7.3, address: "Musterstraße 9"},
+            {id: 2, name: "tom", count: 7.3, address: "Musterstraï¿½e 9"},
             {id: 3, name: "christian", count: 5.2, address: "Silbergasse 14"},
-            {id: 4, name: "robert", count: 5.3, address: "Mariahilferstraße 1"},
+            {id: 4, name: "robert", count: 5.3, address: "Mariahilferstraï¿½e 1"},
             {id: 5, name: "marius", count: 5.4, address: "Musterweg 9"},
             {id: 6, name: "manfred", count: 5.5, address: "Hufeisengasse"}];
 
