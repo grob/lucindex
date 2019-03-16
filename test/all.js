29c16e0ac3e9abebdadd3cb62fc3cfd71440e750
Robert Gaggl <robert@nomatic.org>
2019-03-16 11:41:00 +0100

    updated to lucene 7.7.1
    removed simpleindex: might be re-added sometime in the future, but for
    now keeping it in sync with lucene is too much work (plus i don't
    event know if this is in use somewhere)

--- test/all.js
+++ test/all.js
@@ -1,7 +1,6 @@
 var system = require("system");
 
 exports.testIndex = require("./index_test");
-exports.testSimpleindex = require("./simpleindex_test");
 
 if (require.main == module.id) {
     system.exit(require("test").run.apply(null,
